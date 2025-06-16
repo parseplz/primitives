09dc7301766c75b1db5f14291d8b0852c116e33e
darkseid <gh0stv3g3ta@gmail.com>
2025-06-16 17:40:42 +0530

    fix: cargo fmt errors

--- header-plz/src/lib.rs
+++ header-plz/src/lib.rs
@@ -7,6 +7,6 @@ pub mod error;
 pub mod message_head;
 pub mod methods;
 
-pub use message_head::header_map::header::Header;
 pub use message_head::header_map::HeaderMap;
-pub use message_head::info_line::{request::Request, response::Response, InfoLine};
+pub use message_head::header_map::header::Header;
+pub use message_head::info_line::{InfoLine, request::Request, response::Response};
