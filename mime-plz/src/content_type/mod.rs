97f452c9d0fa43bdc2fa5945c5b1c685eee10cde
darkseid <gh0stv3g3ta@gmail.com>
2025-07-09 10:22:57 +0530

    fix: clippy lint

--- mime-plz/src/content_type/mod.rs
+++ mime-plz/src/content_type/mod.rs
@@ -68,7 +68,7 @@ impl ContentType {
 
 impl Display for ContentType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{self:?}")
     }
 }
 
