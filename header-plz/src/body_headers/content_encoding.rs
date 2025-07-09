1af9324ff46e1bae143c33f38a94f8576768a2e8
darkseid <gh0stv3g3ta@gmail.com>
2025-07-09 13:08:00 +0530

    test: header-plz - content-encoding

--- header-plz/src/body_headers/content_encoding.rs
+++ header-plz/src/body_headers/content_encoding.rs
@@ -9,12 +9,12 @@ pub const ZSTD: &str = "zstd";
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ContentEncoding {
     Brotli,
+    Chunked,
     Compress,
     Deflate,
     Gzip,
     Identity,
     Zstd,
-    Chunked,
     Unknown(String),
 }
 
@@ -22,12 +22,12 @@ impl AsRef<str> for ContentEncoding {
     fn as_ref(&self) -> &str {
         match self {
             ContentEncoding::Brotli => BROTLI,
+            ContentEncoding::Chunked => CHUNKED,
             ContentEncoding::Compress => COMPRESS,
             ContentEncoding::Deflate => DEFLATE,
             ContentEncoding::Gzip => GZIP,
             ContentEncoding::Identity => IDENTITY,
             ContentEncoding::Zstd => ZSTD,
-            ContentEncoding::Chunked => CHUNKED,
             ContentEncoding::Unknown(s) => s,
         }
     }
@@ -37,12 +37,12 @@ impl From<&str> for ContentEncoding {
     fn from(s: &str) -> Self {
         match s {
             BROTLI => ContentEncoding::Brotli,
+            CHUNKED => ContentEncoding::Chunked,
             COMPRESS => ContentEncoding::Compress,
             DEFLATE => ContentEncoding::Deflate,
             GZIP => ContentEncoding::Gzip,
             IDENTITY => ContentEncoding::Identity,
             ZSTD => ContentEncoding::Zstd,
-            CHUNKED => ContentEncoding::Chunked,
             &_ => ContentEncoding::Unknown(s.to_string()),
         }
     }
@@ -54,9 +54,36 @@ mod tests {
 
     #[test]
     fn test_content_encoding_from_str() {
-        let ce = ContentEncoding::Unknown("hola".to_string());
-        assert_eq!(ContentEncoding::from("hola"), ce);
+        let ce = ContentEncoding::from("br");
+        assert_eq!(ce, ContentEncoding::Brotli);
+        assert_eq!(ce.as_ref(), BROTLI);
+
+        let ce = ContentEncoding::from("chunked");
+        assert_eq!(ce, ContentEncoding::Chunked);
+        assert_eq!(ce.as_ref(), CHUNKED);
+
+        let ce = ContentEncoding::from("compress");
+        assert_eq!(ce, ContentEncoding::Compress);
+        assert_eq!(ce.as_ref(), COMPRESS);
+
+        let ce = ContentEncoding::from("deflate");
+        assert_eq!(ce, ContentEncoding::Deflate);
+        assert_eq!(ce.as_ref(), DEFLATE);
+
+        let ce = ContentEncoding::from("gzip");
+        assert_eq!(ce, ContentEncoding::Gzip);
+        assert_eq!(ce.as_ref(), GZIP);
+
+        let ce = ContentEncoding::from("identity");
+        assert_eq!(ce, ContentEncoding::Identity);
+        assert_eq!(ce.as_ref(), IDENTITY);
+
+        let ce = ContentEncoding::from("zstd");
+        assert_eq!(ce, ContentEncoding::Zstd);
+        assert_eq!(ce.as_ref(), ZSTD);
+
+        let ce = ContentEncoding::from("hola");
+        assert_eq!(ce, ContentEncoding::Unknown("hola".to_string()));
         assert_eq!(ce.as_ref(), "hola");
-        dbg!(std::mem::size_of::<ContentEncoding>());
     }
 }
