07594858d6dd07ea03671e123beddcd43e20b556
darkseid <gh0stv3g3ta@gmail.com>
2025-07-15 13:11:49 +0530

    refactor(decompress-plz): decompression logic

---
+++ decompression-plz/src/decompression/mod.rs
@@ -0,0 +1,24 @@
+use std::io::Read;
+
+use brotli::Decompressor;
+use bytes::{BufMut, BytesMut, buf::Writer};
+use header_plz::body_headers::content_encoding::ContentEncoding;
+
+mod decompressors;
+use decompressors::*;
+
+pub fn decompress(
+    mut input: &[u8],
+    writer: &mut Writer<BytesMut>,
+    content_encoding: ContentEncoding,
+) {
+    match content_encoding {
+        ContentEncoding::Brotli => decompress_brotli(input, writer),
+        ContentEncoding::Compress | ContentEncoding::Zstd => decompress_zstd(input, writer),
+        ContentEncoding::Deflate => decompress_deflate(input, writer),
+        ContentEncoding::Gzip => decompress_gzip(input, writer),
+        ContentEncoding::Identity => std::io::copy(&mut input, writer), //.map_err(DecompressError::Identity)
+        ContentEncoding::Chunked => todo!(),
+        ContentEncoding::Unknown(_) => todo!(),
+    };
+}
--- decompression-plz/src/lib.rs
+++ decompression-plz/src/lib.rs
@@ -1,24 +1,2 @@
 #![allow(warnings)]
-use std::io::Read;
-
-use brotli::Decompressor;
-use bytes::{BufMut, BytesMut};
-use header_plz::body_headers::content_encoding::ContentEncoding;
-mod decompressors;
-use decompressors::*;
-
-fn decompress(mut input: &[u8]) {
-    let buf = BytesMut::new();
-    let mut writer = buf.writer();
-    let content_encoding = ContentEncoding::Gzip;
-
-    match content_encoding {
-        ContentEncoding::Brotli => decompress_brotli(input, writer),
-        ContentEncoding::Compress | ContentEncoding::Zstd => decompress_zstd(input, writer),
-        ContentEncoding::Deflate => decompress_deflate(input, writer),
-        ContentEncoding::Gzip => decompress_gzip(input, writer),
-        ContentEncoding::Identity => std::io::copy(&mut input, &mut writer), //.map_err(DecompressError::Identity)
-        ContentEncoding::Chunked => todo!(),
-        ContentEncoding::Unknown(_) => todo!(),
-    };
-}
+mod decompression;
