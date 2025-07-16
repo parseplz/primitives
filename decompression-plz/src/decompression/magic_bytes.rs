b55f3f013d0ad30238e051608eabb44e0889665f
darkseid <gh0stv3g3ta@gmail.com>
2025-07-16 12:44:58 +0530

    test(decompress-plz): fixed tests for magic bytes

--- decompression-plz/src/decompression/magic_bytes.rs
+++ decompression-plz/src/decompression/magic_bytes.rs
@@ -48,11 +48,11 @@ pub fn is_compressed(input: &[u8], encoding: &ContentEncoding) -> bool {
     }
 }
 
-/*
 #[cfg(test)]
 mod tests {
-    use crate::decompression::magic_bytes::is_compressed;
     use header_plz::body_headers::content_encoding::ContentEncoding;
+
+    use crate::decompression::{magic_bytes::is_compressed, single::tests::*};
 
     #[test]
     fn test_magic_bytes_deflate() {
@@ -80,4 +80,3 @@ mod tests {
         ));
     }
 }
-*/
