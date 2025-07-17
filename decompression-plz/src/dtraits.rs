53ccda2309bc0285235287f73e70eda2ff9138b5
darkseid <gh0stv3g3ta@gmail.com>
2025-07-17 16:46:45 +0530

    feat(decompress-plz): added new dtrait methods :

--- decompression-plz/src/dtraits.rs
+++ decompression-plz/src/dtraits.rs
@@ -1,8 +1,11 @@
+use body_plz::variants::Body;
 use header_plz::{HeaderMap, body_headers::BodyHeader};
 
 pub trait DecompressTrait {
-    fn body_headers(&mut self) -> &Option<BodyHeader>;
+    fn get_body(&mut self) -> Body;
+    fn body_headers_as_mut(&mut self) -> &mut Option<BodyHeader>;
 
+    //
     fn header_map_as_mut(&mut self) -> &mut HeaderMap;
 
     fn remove_header_on_position(&mut self, position: usize); // depends - header_map_as_mut
