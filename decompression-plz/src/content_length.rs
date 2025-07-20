5bfb305cf4a09667028556a64e0c07ab0659dbed
darkseid <gh0stv3g3ta@gmail.com>
2025-07-20 11:45:33 +0530

    feat(decompression-plz): added content-length update logic

---
+++ decompression-plz/src/content_length.rs
@@ -0,0 +1,34 @@
+use body_plz::variants::Body;
+use bytes::BytesMut;
+use header_plz::{body_headers::BodyHeader, const_headers::CONTENT_LENGTH};
+
+use crate::dtraits::DecompressTrait;
+
+pub fn add_body_and_update_cl<T>(
+    message: &mut T,
+    body: BytesMut,
+    body_headers: Option<BodyHeader>,
+) where
+    T: DecompressTrait,
+{
+    if !body.is_empty() {
+        update_content_length(message, body.len());
+    }
+
+    if let Some(bh) = body_headers {
+        message.body_headers_as_mut().replace(bh);
+    }
+    message.set_body(Body::Raw(body));
+}
+
+pub fn update_content_length<T>(message: &mut T, len: usize)
+where
+    T: DecompressTrait,
+{
+    let len_string = len.to_string();
+    match message.has_header_key(CONTENT_LENGTH) {
+        Some(pos) => message.update_header_value_on_position(pos, &len_string),
+        None => message.add_header(CONTENT_LENGTH, len_string.as_str()),
+    }
+}
+
