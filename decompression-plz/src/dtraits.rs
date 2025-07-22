b8828977fed02562da66dbf86782b958824d26cb
darkseid <gh0stv3g3ta@gmail.com>
2025-07-22 09:51:24 +0530

    feat(decompression-plz): added remove_header_on_position and truncate_header_value_on_position to DecompressTrait

--- decompression-plz/src/dtraits.rs
+++ decompression-plz/src/dtraits.rs
@@ -2,16 +2,23 @@ use body_plz::variants::Body;
 use header_plz::{Header, HeaderMap, body_headers::BodyHeader};
 
 pub trait DecompressTrait {
+    // Body
     fn get_body(&mut self) -> Body;
+    fn set_body(&mut self, body: Body);
 
     fn body_headers_as_mut(&mut self) -> &mut Option<BodyHeader>;
 
+    /// header
     fn header_map(&self) -> &HeaderMap;
 
     fn header_map_as_mut(&mut self) -> &mut HeaderMap;
 
     fn has_header_key(&self, key: &str) -> Option<usize> {
         self.header_map().header_key_position(key)
+    }
+
+    fn remove_header_on_position(&mut self, position: usize) {
+        self.header_map_as_mut().remove_header_on_position(position);
     }
 
     fn update_header_value_on_position(&mut self, pos: usize, value: &str) {
@@ -23,10 +30,17 @@ pub trait DecompressTrait {
         self.header_map_as_mut().add_header(header);
     }
 
-    fn set_body(&mut self, body: Body);
+    fn truncate_header_value_on_position<T>(
+        &mut self,
+        pos: usize,
+        truncate_at: T,
+    ) where
+        T: AsRef<str>,
+    {
+        self.header_map_as_mut()
+            .truncate_header_value_on_position(pos, truncate_at);
+    }
 
-    //fn remove_header_on_position(&mut self, position: usize); // depends - header_map_as_mut
-    //
     //fn update_header_value_on_position(
     //    &mut self,
     //    position: usize,
