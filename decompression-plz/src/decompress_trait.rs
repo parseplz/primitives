259ef315ebadebe2658af393e235412787d09c3c
darkseid <gh0stv3g3ta@gmail.com>
2025-07-24 15:39:34 +0530

    feat(decompression-plz): added some default helper trait methods

--- decompression-plz/src/decompress_trait.rs
+++ decompression-plz/src/decompress_trait.rs
@@ -13,21 +13,31 @@ pub trait DecompressTrait {
 
     fn header_map_as_mut(&mut self) -> &mut HeaderMap;
 
-    fn has_header_key(&self, key: &str) -> Option<usize> {
-        self.header_map().header_key_position(key)
+    // entire header
+    fn add_header(&mut self, key: &str, value: &str) {
+        let header: Header = (key, value).into();
+        self.header_map_as_mut().add_header(header);
     }
 
+    fn add_multi_headers(&mut self, mut headers: Vec<Header>) {
+        self.header_map_as_mut().headers_as_mut().append(&mut headers);
+    }
     fn remove_header_on_position(&mut self, position: usize) {
         self.header_map_as_mut().remove_header_on_position(position);
     }
 
-    fn update_header_value_on_position(&mut self, pos: usize, value: &str) {
-        self.header_map_as_mut().update_header_value_on_position(pos, value);
+    // key
+    fn has_header_key(&self, key: &str) -> Option<usize> {
+        self.header_map().header_key_position(key)
     }
 
-    fn add_header(&mut self, key: &str, value: &str) {
-        let header: Header = (key, value).into();
-        self.header_map_as_mut().add_header(header);
+    fn remove_header_on_key(&mut self, key: &str) -> bool {
+        self.header_map_as_mut().remove_header_on_key(key)
+    }
+
+    // value
+    fn update_header_value_on_position(&mut self, pos: usize, value: &str) {
+        self.header_map_as_mut().update_header_value_on_position(pos, value);
     }
 
     fn truncate_header_value_on_position<T>(
