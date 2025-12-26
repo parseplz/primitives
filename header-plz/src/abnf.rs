30c50d633cdf0d98eaffcfe27152d01c04ca1c7d
darkseid <gh0stv3g3ta@gmail.com>
2025-12-26 09:17:16 +0530

    feat(header-plz): changed char to u8 ABNF

--- header-plz/src/abnf.rs
+++ header-plz/src/abnf.rs
@@ -1,8 +1,8 @@
-pub const COLON: char = ':';
+pub const COLON: u8 = b':';
 pub const COMMA: char = ',';
 pub const CRLF: &str = "\r\n";
 pub const FORWARD_SLASH: &[u8] = b"/";
 pub const HEADER_DELIMITER: &[u8; 4] = b"\r\n\r\n";
 pub const HEADER_FS: &str = ": ";
-pub const LF: &str = "\n";
-pub const SP: char = ' ';
+pub const LF: u8 = b'\n';
+pub const SP: u8 = b' ';
