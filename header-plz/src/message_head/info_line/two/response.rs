aaa6035780d75ae684aba05b021082ff04311cf3
darkseid <gh0stv3g3ta@gmail.com>
2026-01-14 16:42:06 +0530

    feat(header-plz): use own StatusCode in ResponseLine and implemented setters and getters

--- header-plz/src/message_head/info_line/two/response.rs
+++ header-plz/src/message_head/info_line/two/response.rs
@@ -1,12 +1,28 @@
-use http::StatusCode;
+use crate::status::StatusCode;
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct ResponseLine {
     status: StatusCode,
+}
+
+impl Default for ResponseLine {
+    fn default() -> Self {
+        Self {
+            status: StatusCode::from_u16(200).unwrap(),
+        }
+    }
 }
 
 impl ResponseLine {
     pub fn into_parts(self) -> StatusCode {
         self.status
+    }
+
+    pub fn set_status(&mut self, status: StatusCode) {
+        self.status = status
+    }
+
+    pub fn status(&self) -> &StatusCode {
+        &self.status
     }
 }
