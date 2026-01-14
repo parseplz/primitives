d6e0d042dea9562e29c8973002ffd6cb303c7d15
darkseid <gh0stv3g3ta@gmail.com>
2026-01-14 16:43:31 +0530

    feat(header-plz): implemented getters and setters for Two::RequestLine

--- header-plz/src/message_head/info_line/two/request.rs
+++ header-plz/src/message_head/info_line/two/request.rs
@@ -14,7 +14,29 @@ impl RequestLine {
         (self.method, self.uri)
     }
 
+    // getters
+    pub fn method(&self) -> &Method {
+        &self.method
+    }
+
+    pub fn uri(&self) -> &Uri {
+        &self.uri
+    }
+
     pub fn extension(&self) -> Option<&Bytes> {
         self.extension.as_deref()
+    }
+
+    // setters
+    pub fn set_method(&mut self, method: Method) {
+        self.method = method
+    }
+
+    pub fn set_uri(&mut self, uri: Uri) {
+        self.uri = uri
+    }
+
+    pub fn set_extension(&mut self, ext: Bytes) {
+        self.extension = Some(Box::new(ext));
     }
 }
